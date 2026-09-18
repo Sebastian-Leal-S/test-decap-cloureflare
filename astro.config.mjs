1bb15edb4c0e6a6082be6e7e6da21edce3676f63
Sebastian Leal S <sebas.leal.ti@gmail.com>
2026-09-17 22:42:29 -0500

    fix: update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://test-decap-cloureflare.sedastianleal.workers.dev/',
   output: 'server',
   adapter: cloudflare(),
   integrations: [mdx(), sitemap()],
